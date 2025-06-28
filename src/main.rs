a26fdd5a2c4f3745746fa2f6dec5ec2f895198b2
Bastian Schubert <cemoktra@github.com>
2025-06-28 18:44:59 +0200

    update deps

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,12 @@ mod dht11;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let mut serial = arduino_hal::Usart::new(
+        dp.USART0,
+        pins.d0,
+        pins.d1.into_output(),
+        arduino_hal::hal::usart::BaudrateArduinoExt::into_baudrate(57600),
+    );
 
     let mut led = pins.d13.into_output();
     led.set_high();
